d4085cb02ed6c5eda3fda7b9b4cb84efb5a2a141
KiddHermez <dayvidnavarrete@gmail.com>
2023-07-01 22:25:12 -0500

    Better Desing: Colors and Position in Desktop

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,20 @@ module.exports = {
             fontFamily: {
                 Montserrat: ['Montserrat', 'sans-serif'],
             },
+            colors: {
+                poke: {
+                    black: '#afafaf',
+                    blue: '#7ad7d7',
+                    brown: '#e4aa79',
+                    gray: '#d0d0d0',
+                    green: '#7ad788',
+                    pink: '#e18b89',
+                    purple: '#a97ad7',
+                    red: '#e07187',
+                    white: '#ebebeb',
+                    yellow: '#e6d27a',
+                },
+            },
         },
     },
     plugins: [require('tailwind-scrollbar')],
