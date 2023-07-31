104b8c263ff321f3bc1249d2344cd6adc24762b2
kiddhermez <dayvidnavarrete@gmail.com>
2023-07-31 17:58:16 -0500

    Create Routing

---
+++ src/api/interfaces/PokemonsByGeneration.ts
@@ -0,0 +1,8 @@
+export interface PokemonsByGeneration {
+    pokemon_v2_pokemonspecies: PokemonV2Pokemonspecy[];
+}
+
+export interface PokemonV2Pokemonspecy {
+    id: number;
+    name: string;
+}
